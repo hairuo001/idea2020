7da36fb46f2386039e4a2d2745f4b68b5f173e73
harry <harry@atguigu.com>
2020-05-06 21:55:34 +0800

    to github

--- src/main/java/com/atguigu/jedipool/JedisPoolTest.java
+++ src/main/java/com/atguigu/jedipool/JedisPoolTest.java
@@ -32,8 +32,10 @@ public class JedisPoolTest {
         System.out.println(jedis.get("JedisPool"));
 
         //6.关闭连接池
-        jedisPool.close();// edit with hostfix to make conflict! 2
+        jedisPool.close();// edit with hostfix to make conflict!
 
         //edit in version hotfix
+
+        //to github
     }
 }
